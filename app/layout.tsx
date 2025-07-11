05455e532c3f982affde9f81a4ca45621c058d82
manavlade <manavlade14690@gmail.com>
2025-07-11 13:29:10 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Doctor Admin panel',
+  description: 'Created with next.js and Tailwind CSS',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
